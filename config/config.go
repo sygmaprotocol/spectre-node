1594a8a0139618d2430df4c1eb69a937a5d790f1
mpetrun5 <matija.petrunic@gmail.com>
2023-10-19 13:43:52 +0200

    Add basic node config

--- config/config.go
+++ config/config.go
@@ -1,3 +1,25 @@
 package config
 
+import "github.com/kelseyhightower/envconfig"
+
 const PREFIX = "SPECTRE"
+
+type Config struct {
+	Observability
+}
+
+type Observability struct {
+	LogLevel string `default:"debug" split_words:"true"`
+	LogFile  string `default:"out.log" split_words:"true"`
+}
+
+// LoadConfig loads config from the environment and validates the fields
+func LoadConfig() (*Config, error) {
+	var c Config
+	err := envconfig.Process(PREFIX, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
---
+++ config/config_test.go
@@ -0,0 +1,44 @@
+package config_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/sygmaprotocol/spectre-node/config"
+)
+
+type ConfigTestSuite struct {
+	suite.Suite
+}
+
+func TestRunConfigTestSuite(t *testing.T) {
+	suite.Run(t, new(ConfigTestSuite))
+}
+
+func (s *ConfigTestSuite) Test_LoadConfig_DefaultValues() {
+	c, err := config.LoadConfig()
+
+	s.Nil(err)
+	s.Equal(c, &config.Config{
+		Observability: config.Observability{
+			LogLevel: "debug",
+			LogFile:  "out.log",
+		},
+	})
+}
+
+func (s *ConfigTestSuite) Test_LoadEVMConfig_SuccessfulLoad() {
+	os.Setenv("SPECTRE_LOG_LEVEL", "info")
+	os.Setenv("SPECTRE_LOG_FILE", "out2.log")
+
+	c, err := config.LoadConfig()
+
+	s.Nil(err)
+	s.Equal(c, &config.Config{
+		Observability: config.Observability{
+			LogLevel: "info",
+			LogFile:  "out2.log",
+		},
+	})
+}
