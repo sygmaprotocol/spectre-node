546eaa6e6a9bb7fbf85efc07888c60bdc691816d
mpetrun5 <matija.petrunic@gmail.com>
2023-11-15 12:57:21 +0100

    Add calls to the prover

--- chains/evm/prover/prover.go
+++ chains/evm/prover/prover.go
@@ -3,6 +3,7 @@ package prover
 import (
 	"context"
 	"fmt"
+	"net/rpc"
 
 	"github.com/attestantio/go-eth2-client/api"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
@@ -10,12 +11,14 @@ import (
 )
 
 type StepArgs struct {
+	Spec    Spec
 	Pubkeys [512][48]byte
 	Domain  phase0.Domain
 	Update  *consensus.LightClientFinalityUpdateCapella
 }
 
 type RotateArgs struct {
+	Spec   Spec
 	Update *consensus.LightClientUpdateCapella
 }
 
@@ -38,18 +41,34 @@ type BeaconClient interface {
 type Prover struct {
 	lightClient  LightClient
 	beaconClient BeaconClient
+	proverClient *rpc.Client
 
+	spec          Spec
 	epochSize     uint64
 	committeeSize uint64
 }
 
-func NewProver(beaconClient BeaconClient, lightClient LightClient, epochSize uint64, committeeSize uint64) *Prover {
+func NewProver(
+	proverUrl string,
+	beaconClient BeaconClient,
+	lightClient LightClient,
+	spec Spec,
+	epochSize uint64,
+	committeeSize uint64,
+) (*Prover, error) {
+	client, err := rpc.DialHTTP("tcp", proverUrl)
+	if err != nil {
+		return nil, err
+	}
+
 	return &Prover{
+		proverClient:  client,
+		spec:          spec,
 		epochSize:     epochSize,
 		committeeSize: committeeSize,
 		beaconClient:  beaconClient,
 		lightClient:   lightClient,
-	}
+	}, nil
 }
 
 // StepProof generates the proof for the sync step
@@ -58,9 +77,30 @@ func (p *Prover) StepProof() (*EvmProof, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(args)
 
-	return nil, nil
+	var proof *EvmProof
+	err = p.proverClient.Call("genEvmProofAndInstancesStepSyncCircuit", args, &proof)
+	if err != nil {
+		return nil, err
+	}
+
+	return proof, nil
+}
+
+// RotateProof generates the proof for the sync committee rotation for the period
+func (p *Prover) RotateProof(slot uint64) (*EvmProof, error) {
+	args, err := p.rotateArgs(slot)
+	if err != nil {
+		return nil, err
+	}
+
+	var proof *EvmProof
+	err = p.proverClient.Call("genEvmProofAndInstancesRotationCircuit", args, &proof)
+	if err != nil {
+		return nil, err
+	}
+
+	return proof, nil
 }
 
 func (p *Prover) stepArgs() (*StepArgs, error) {
@@ -89,17 +129,8 @@ func (p *Prover) stepArgs() (*StepArgs, error) {
 		Pubkeys: pubkeys,
 		Domain:  domain,
 		Update:  update,
+		Spec:    p.spec,
 	}, nil
-}
-
-func (p *Prover) RotateProof(slot uint64) (*EvmProof, error) {
-	args, err := p.rotateArgs(slot)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println(args)
-
-	return nil, nil
 }
 
 func (p *Prover) rotateArgs(slot uint64) (*RotateArgs, error) {
@@ -114,5 +145,6 @@ func (p *Prover) rotateArgs(slot uint64) (*RotateArgs, error) {
 	update := updates[0]
 	return &RotateArgs{
 		Update: update,
+		Spec:   p.spec,
 	}, nil
 }
---
+++ chains/evm/prover/spec.go
@@ -0,0 +1,9 @@
+package prover
+
+type Spec string
+
+const (
+	TESTNET_SPEC Spec = "testnet"
+	MINIMAL_SPEC Spec = "minimal"
+	MAINNET_SPEC Spec = "mainnet"
+)
