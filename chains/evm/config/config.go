4b91b551aa6a6a6873e922df4613dd01d93530af
Matija Petrunić <matija.petrunic@gmail.com>
2024-09-16 14:50:07 +0200

    feat: enable execute only networks (#44)

--- chains/evm/config/config.go
+++ chains/evm/config/config.go
@@ -12,9 +12,9 @@ import (
 
 type EVMConfig struct {
 	config.BaseNetworkConfig
-	BeaconEndpoint        string  `required:"true" split_words:"true"`
-	Router                string  `required:"true"`
-	Spectre               string  `required:"true"`
+	BeaconEndpoint        string `split_words:"true"`
+	Router                string
+	Spectre               string
 	Spec                  string  `default:"mainnet"`
 	MaxGasPrice           int64   `default:"500000000000" split_words:"true"`
 	GasMultiplier         float64 `default:"1" split_words:"true"`
@@ -25,6 +25,7 @@ type EVMConfig struct {
 	ForcePeriod           bool    `default:"false" split_words:"true"`
 	FinalityThreshold     uint64  `default:"342" split_words:"true"`
 	SlotsPerEpoch         uint64  `default:"32" split_words:"true"`
+	TargetDomains         []int16 `split_words:"true"`
 }
 
 // LoadEVMConfig loads EVM config from the environment and validates the fields
